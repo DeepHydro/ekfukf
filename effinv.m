fc109b05fd1dce98638e5638d367f9cebef8de92
unoe <umberto.noe1992@gmail.com>
2017-01-17 12:36:26 +0000

    Documentation

--- effinv.m
+++ effinv.m
@@ -2,6 +2,8 @@ function invA = effinv(A)
 % EFFINV    Helper function to perform numerically stable inverse
 % 
 %   invA = effinv(A)
+% 
+% Umberto Noe, January 2017
 
 [m, n] = size(A);
 invA   = A \ eye(m, n);
